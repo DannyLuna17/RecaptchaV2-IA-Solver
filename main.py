73de22cf6f328aaead707b16f6957858f4d51287
Luna <69711934+LunaPy17@users.noreply.github.com>
2023-10-05 19:20:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from recaptchaSolver import solver
 
 if __name__ == "__main__":
-    pritn(solver("https://google.com/recaptcha/api2/demo", proxy=None))
+    pritn(solver("https://google.com/recaptcha/api2/demo", proxy=None, verbose=True, headless=False))
